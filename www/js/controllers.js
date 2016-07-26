2959658c065c80f6dd98e2ad050cd8fe8f627dc5
valentinCro <valentin.crochemore1@etu.univ-rouen.fr>
2016-07-26 19:18:00 +0200

    feature (home screen) : add in time activity and display it

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,93 +2,60 @@ angular.module('starter.controllers', [])
 
 .controller('AccueilCtrl', function($scope, $interval) {
 
-  $scope.hour = 0;
-  $scope.minute = 0;
-  $scope.second = 0;
-  $scope.started = false;
+  //VARIABLE DECLARATION
+  $scope.activityStartedTimes = [];
+  $scope.activities = [];
   $scope.paused = false;
-  $scope.timer = null;
-
-  $scope.startTimer = function() {
-    $scope.timer = $interval(function() {
-      if ($scope.second === 60) {
-        $scope.second = 0;
-        if ($scope.minute === 60) {
-          $scope.minute = 0;
-          $scope.hour++;
-        } else {
-          $scope.minute++;
-        }
-
-      } else {
-        $scope.second++;
-      }
-    }, 1000);
+  //FUNCTION
+  $scope.startNewActivity = function() {
+    $scope.activityStartedTimes.push(new Date());
   };
 
-  $scope.start = function() {
-    if ($scope.timer == null) {
-      $scope.startTimer();
+  $scope.stopNewActivity = function() {
+    $scope.activityStartedTimes.push(new Date());
+    var duration = 0;
+    var i = 0;
+    while(i < $scope.activityStartedTimes.length) {
+      var tmp = $scope.activityStartedTimes[i + 1] - $scope.activityStartedTimes[i];
+      tmp = tmp/1000;
+      console.log(tmp);
+      tmp = (tmp - (tmp % 60)) / 60;
+      console.log(tmp);
+      tmp = (tmp - (tmp % 60)) / 60;
+      console.log(tmp);
+      duration = tmp;
+      i += 2;
     }
-
-    if ($scope.started) {
-      $scope.paused = false;
-      $scope.startTimer();
-    } else {
-      $scope.started = true;
-    }
+    var activity = {
+        number : "",
+        name : "",
+        durationInHour : duration,
+        startingDate : $scope.activityStartedTimes[0],
+        endingDate : $scope.activityStartedTimes[$scope.activityStartedTimes.length - 1],
+        date : $scope.activityStartedTimes[0]
+    };
+    $scope.activities.push(activity);
+    $scope.activityStartedTimes = [];
   };
 
-  $scope.reload = function() {
-    $interval.cancel($scope.timer);
-    $scope.hour = 0;
-    $scope.minute = 0;
-    $scope.second = 0;
-    $scope.startTimer();
-  };
-
-  $scope.pause = function() {
-    $interval.cancel($scope.timer);
+  $scope.takeABreak = function() {
+    $scope.activityStartedTimes.push(new Date());
     $scope.paused = true;
   };
 
-  $scope.stop = function() {
-    $interval.cancel($scope.timer);
-    $scope.timer = null;
-    $scope.started = false;
+  $scope.continue = function() {
+    $scope.activityStartedTimes.push(new Date());
     $scope.paused = false;
-    $scope.hour = 0;
-    $scope.minute = 0;
-    $scope.second = 0;
   };
 
-  $scope.isStarted = function() {
-    return $scope.started;
+  //GETTER & SETTER
+  $scope.getActivityStartedTimes = function() {
+    return $scope.activityStartedTimes;
   };
 
-  $scope.isPaused = function() {
-    return $scope.paused;
-  };
 
-  $scope.getHour = function() {
-    if ($scope.hour < 10) {
-      return '0' + $scope.hour;
-    }
-    return $scope.hour;
-  };
-
-  $scope.getMinute = function() {
-    if ($scope.minute < 10) {
-      return '0' + $scope.minute;
-    }
-    return $scope.minute;
-  };
-
-  $scope.getSecond = function() {
-    if ($scope.second < 10) {
-      return '0' + $scope.second;
-    }
-    return $scope.second;
+  $scope.getActivities = function() {
+    return $scope.activities;
   };
 
 })
